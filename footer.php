7d70e7a8e88dd73987d78f3b630026fb188fc2f9
poornimak1227 <106701465+poornimak1227@users.noreply.github.com>
2022-06-02 00:53:35 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 Brought to You By  <a href="#">Vinay And Team</a></strong>
+</footer>
